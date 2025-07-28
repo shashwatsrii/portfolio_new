501e00e0b52a92f3fabe933f057fad8d0d4e0b3f
Shashwat Srivastava <shashwatswork@gmail.com>
2025-07-29 02:00:51 +0530

    fix

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
       opacity: 1,
       transition: {
         duration: 0.8,
-        ease: [0.6, -0.05, 0.01, 0.99]
+        ease: "easeOut"
       }
     }
   }
